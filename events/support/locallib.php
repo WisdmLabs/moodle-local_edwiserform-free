c5797603a149bb8537f3662c9a7b768e0a374911
yogesh.shirsath@wisdmlabs.com <yogesh.shirsath@wisdmlabs.com>
2018-09-11 12:32:34 +0530

    refs #47061 Created support form

---
+++ events/support/locallib.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     edwiserformevent_support
+ * @copyright   2018 WisdmLabs <support@wisdmlabs.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author      Yogesh Shirsath
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/local/edwiserform/events/events.php');
+
+class edwiserform_events_support extends edwiserform_events_plugin {
+
+    public function submission_email_message($form, $submission) {
+        return parent::common_submission_email_message($form, $submission);
+    }
+}
