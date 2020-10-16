67be58329637a04969eca21d283adc8a194def37
Yogesh <yogesh.shirsath@wisdmlabs.com>
2020-10-16 16:42:04 +0530

    Updated version.php for 1.2.0

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_edwiserform';
-$plugin->release = '1.1.1';
-$plugin->version  = 2019111503;
+$plugin->release = '1.2.0';
+$plugin->version  = 2020101600;
 $plugin->requires = 2016052314;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 3600;
