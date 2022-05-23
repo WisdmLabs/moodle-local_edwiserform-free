63a3961a1b0c285728f3193f8eb0f59d8900c4e1
Yogesh Shirsath <yogesh.shirsath@wisdmlabs.com>
2022-05-23 22:36:44 +0530

    Updated gulp task

--- tasks/style.js
+++ tasks/style.js
@@ -11,29 +11,27 @@ var sassOptions = {
 
 gulp.task('commoncss', function() {
     return gulp.src(['assets/scss/common/styles.scss'])
-    .pipe(sass(sassOptions))
-    .pipe(gulp.dest('.'));
+        .pipe(sass(sassOptions))
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task('separatecss', function(done) {
     gulp.src([
-        'assets/scss/common/*.scss',
-        '!assets/scss/common/styles.scss',
-        'assets/scss/formedit/*.scss',
-        'assets/scss/*.scss'
-    ])
-    .pipe(sass(sassOptions))
-    .pipe(gulp.dest('./style'))
-    .pipe(notify({message: 'Common styles completed.', onLast: true}));
+            'assets/scss/common/*.scss',
+            'assets/scss/*.scss'
+        ])
+        .pipe(sass(sassOptions))
+        .pipe(gulp.dest('./style'))
+        .pipe(notify({ message: 'Common styles completed.', onLast: true }));
     done();
 });
 
 gulp.task('copydatatablescss', function() {
     return gulp.src([
-        'node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css',
-        'node_modules/datatables.net-buttons-bs4/css/buttons.bootstrap4.css',
-        'node_modules/datatables.net-fixedcolumns-bs4/css/fixedColumns.bootstrap4.css'
-    ])
-    .pipe(concat('datatables.scss'))
-    .pipe(gulp.dest('assets/scss'));
+            'node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css',
+            'node_modules/datatables.net-buttons-bs4/css/buttons.bootstrap4.css',
+            'node_modules/datatables.net-fixedcolumns-bs4/css/fixedColumns.bootstrap4.css'
+        ])
+        .pipe(concat('datatables.scss'))
+        .pipe(gulp.dest('assets/scss'));
 });
