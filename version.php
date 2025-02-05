b6ab5ac248bf0c86bf3d1540b52b31cd86f865a1
jaydeeptrivedi13 <jaydeep.trivedi@wisdmlabs.com>
2025-02-05 11:24:56 +0530

    :zap: Fixed delete modal location css issue

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_edwiserform';
-$plugin->release = '1.3.0';
-$plugin->version  = 2022052401;
+$plugin->release = '1.3.1';
+$plugin->version  = 2025020400;
 $plugin->requires = 2016052314;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 3600;
