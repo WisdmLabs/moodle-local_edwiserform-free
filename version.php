d67036824d727aefa2bb031cd16a4257de4647dd
Ujjawal Srivastava <ujjawal@example.com>
2026-05-07 14:51:45 +0530

    :zap: Bumped version to 1.3.4 and updated release date in CHANGES.txt

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_edwiserform';
-$plugin->release = '1.3.3';
-$plugin->version  = 2025080700;
+$plugin->release = '1.3.4';
+$plugin->version  = 2026050700;
 $plugin->requires = 2016052314;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 3600;
